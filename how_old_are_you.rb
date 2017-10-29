701f3353a7364967a97a27499721fa42b1c5d9d0
Jay Porta <jasonmatthewporta@gmail.com>
2017-10-29 12:16:17 +0000

    Done.

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,5 +1,4 @@
 require_relative './current_age_for_birth_year.rb'
-<<<<<<< HEAD
 
 puts "What year were you born?"
 birth_year = gets.to_i
@@ -7,12 +6,10 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
 
 puts "You are: " + users_age.to_s + " years old."
-=======
- 
+
 puts "What year were you born?"
 birth_year = gets.to_i
- 
+
 users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
- 
+
 puts "You are: " + users_age.to_s + " years old."
->>>>>>> f8ca81add7533b80e5b585ce479909be5a9e87c2
