25be4b28e2546b4091006754e04387c096ccf886
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-02 01:07:40 +0000

    Done.

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,8 +1,7 @@
 def current_age_for_birth_year(birth_year)
-      2003 - birth_year
+      Time.now.year - birth_year
 end
 
 age_of_person = current_age_for_birth_year(1984)
 
-puts age_of_person
 
