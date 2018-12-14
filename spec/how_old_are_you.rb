78821e629cea28b17187d33c1a254f81b5b55be0
Burak Kocak <burakkocak884@ghotmail.com>
2018-12-13 23:11:19 -0500

    Done.

---
+++ spec/how_old_are_you.rb
@@ -0,0 +1,6 @@
+def current_age_for_birth_year(birth_year)
+	puts "What year were you born?"
+	birth_year = gets.to_i
+	users_age = current_age_for_birth_year(birth_year)
+	puts "You are: " + users_age.to_s + " years old."
+end
