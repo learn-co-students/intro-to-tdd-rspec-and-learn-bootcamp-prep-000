1c3528aa582de864c963fabd452e865441bbc750
Adrian CIMPAN <adrian28c@yahoo.com>
2019-03-16 16:38:38 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,13 @@
+require_relative '../current_age_of_birth_year.rb'
+ def current_age_of_birth_year(birth_year)
+    2003 - birth_year
+ end
+ 
+ describe "current_age_of_birth_year" do
+ it "returns the age of a person based on the year of birth" do
+     age_of_person = current_age_of_birth_year(1984)
+     
+     expect(age_of_person) .to eq(19)
+end
+   
+   
