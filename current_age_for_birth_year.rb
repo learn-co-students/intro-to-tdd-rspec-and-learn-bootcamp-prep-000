a9368498b66b0b7e7db2fcc8f485a6fdd675a6dc
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-21 10:58:22 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,10 @@
+current_year = 2003
+current_age_for_birth_year = 1984
+
+def current_age_for_birth_year
+age_of_person = current_year.to_i - current_age_for_birth_year.to_i
+  puts "It's 2003 now. You are #{age_of_person} years old"
+end
+
+current_age_for_birth_year(1984)
+
