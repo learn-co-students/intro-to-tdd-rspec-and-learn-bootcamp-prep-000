a9c572091ce662cb21ac123e38bb0acea8fc150e
Ladeeluc <laura.hatch.lh@gmail.com>
2020-02-13 12:59:29 -0600

    Done.

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -4,6 +4,10 @@
 # def current_age_for_birth_year(birth_year)
 # end
 
+# def current_age_for_birth_year(birth_year)
+#     2020 - birth_year
+#   end
+
 def current_age_for_birth_year(birth_year)
-    2020 - birth_year
-  end
+  Time.now.year - birth_year
+end
