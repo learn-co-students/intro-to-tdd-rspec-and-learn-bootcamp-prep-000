22779e01e0ec5bf07a0be4f36d40c884b45ae275
Edwin Cyr <edmundopierre@yahoo.com>
2020-03-02 22:08:09 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,6 @@
+def current_age_for_birth_year(year)
+    current_year = Time.new.year
+    birth_year = 2001
+    answer = current_year - birth_year
+end
+age_of_person = current_age_for_birth_year(1984)
